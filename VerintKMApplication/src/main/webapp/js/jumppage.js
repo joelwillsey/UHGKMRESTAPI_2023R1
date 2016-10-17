61445adc9a5329abcc4774b43444e58e57c5c605
eraygorodetskiy <eraygorodetskiy@r2059  eraygorodetskiy  2016-12-22 16:07:30 -0600 (Thu, 22 Dec 2016)>
2016-10-17 14:51:49 +0000

    Added in a few parses for different parts of the ISET things.
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@1657 b552034e-f646-462b-9072-8d94b4fb3fa1

--- VerintKMApplication/src/main/webapp/js/jumppage.js
+++ VerintKMApplication/src/main/webapp/js/jumppage.js
@@ -37,10 +37,14 @@ $(document).ready(function() {
 	// Get all the iset return from the hopefully working service
 	$.fn.getIsetResponse = function(refName, objType, objId) {
 		
+		var migratableReferenceId = "";
+		
 		var url = contentServiceName + 'km/iset/migref?refName='+refName+'&objType='+objType+'&objID='+objId;
 		$.fn.serviceCall('GET', '', url, 15000, function(data) {
-			//$.fn.parseTags(data);
+			migratableReferenceId = data.migratableReferenceId[0].migratableReferenceId;
 		});
+		return migratableReferenceId;
+	}
 	
 	$.fn.callingRespectiveService = function( variables) {
 		
@@ -60,44 +64,96 @@ $(document).ready(function() {
 			case "report":
 				if(variables[3].substring(0,2) == "p{"){
 					var report = variables[3].substring(2, variables[3].length-1);
+
 					//run a search for problem object on UHG and Problem GUID
+					var migratableReferenceId = $.fn.getIsetResponse("","Solution",report);
+					var url = contentServiceName + 'content_container.html? id='+migratableReferenceId;
+					window.location.replace(url);
+					break;
+					
 				}else if(variables[3].substring(0,2) == "p["){
 					var report = variables[3].substring(2, variables[3].length-1);
+					
 					//run a search for problem object on UHG and Problem RefID
+					var migratableReferenceId = $.fn.getIsetResponse(report,"Solution","");
+					var url = contentServiceName + 'content_container.html? id='+migratableReferenceId;
+					window.location.replace(url);
+					break;
+					
 				}else if(variables[3].substring(0,2) == "p("){
 					var report = variables[3].substring(2, variables[3].length-1);
 					
-					var url = "http://localhost:8090/searchservices/search_container.html?tags=" + UHG + ","+report+"\"";
+					var url = searchServiceName + "searchservices/search_container.html?tags=" + UHG + ","+report+"\"";
 					window.location.replace(url);
+					break;
+					
 				}else if(variables[3].substring(0,2) == "d{"){
 					var report = variables[3].substring(2, variables[3].length-1);
+					
 					//run a search for decision tree on UHG and Dtree GUID
+					var migratableReferenceId = $.fn.getIsetResponse("","Document",report);
+					var url = contentServiceName + 'content_container.html? id=' + migratableReferenceId;
+					window.location.replace(url);
+					break;
+
 				}else if(variables[3].substring(0,2) == "d["){
 					var report = variables[3].substring(2, variables[3].length-1);
+					
 					//run a search for decision tree on UHG and Dtree RefID
+					var migratableReferenceId = $.fn.getIsetResponse(report,"Document","");
+					var url = contentServiceName + 'content_container.html? id=' + migratableReferenceId;
+					window.location.replace(url);
+					break;
+					
 				}else if(variables[3].substring(0,2) == "d("){
 					var report = variables[3].substring(2, variables[3].length-1);
+					
 					//run a search for decision tree on UHG and Migratable Reference
+					var url = contentServiceName + 'content_container.html? id=' + report;
+					window.location.replace(url);
+					break;
+					
 				}
 				break;
 			case "view()":
 				if(variables[3].substring(0,2) == "r{"){
 					var report = variables[3].substring(2, variables[3].length-1);
+					
 					//run a search for content on UHG and Case GUID
+					var migratableReferenceId = $.fn.getIsetResponse("","Case",report);
+					var url = contentServiceName + 'content_container.html? id=' + migratableReferenceId;
+					window.location.replace(url);
+					break;
+					
 				}else if(variables[3].substring(0,2) == "r["){
 					var report = variables[3].substring(2, variables[3].length-1);
+					
 					//run a search for content on UHG and Case RefID
+					var migratableReferenceId = $.fn.getIsetResponse(report,"Case","");
+					var url = contentServiceName + 'content_container.html? id=' + migratableReferenceId;
+					window.location.replace(url);
+					break;
+					
 				}else if(variables[3].substring(0,2) == "r("){
 					var report = variables[3].substring(2, variables[3].length-1);
+					
 					//run a search for content on UHG and Migratable Reference
+					var url = contentServiceName + 'content_container.html? id=' + report;
+					window.location.replace(url);
+					break;
+					
 				}
 				break;
 			case "Report":
 				var reportSplit = variables[3].split(":");
 				var content = reportSplit[0].substring(2, reportSplit[0].length-1);
 				var contentFaq = reportSplit[1];
+				
 				//apply filter to result list with UHG, content, and contentFAQ
+				var url = searchServiceName + "searchservices/search_container.html?tags=" + UHG + "&contentType="+content+"&search="+contentFaq+"\"";
+				window.location.replace(url);
 				break;
+				
 			case "searchProperties":
 				
 				var searchProperties = variables[3];
@@ -109,7 +165,6 @@ $(document).ready(function() {
 				break;
 			}
 		}
-	}
 	}
 	
 	
