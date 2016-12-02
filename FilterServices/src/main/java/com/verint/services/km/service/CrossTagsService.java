011aa1ea9a658e0b346b0a4a4aacde182a405e78
eraygorodetskiy <eraygorodetskiy@r2059  eraygorodetskiy  2016-12-22 16:07:30 -0600 (Thu, 22 Dec 2016)>
2016-12-02 22:42:43 +0000

    Miller's changes from basic.
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@1917 b552034e-f646-462b-9072-8d94b4fb3fa1

--- FilterServices/src/main/java/com/verint/services/km/service/CrossTagsService.java
+++ FilterServices/src/main/java/com/verint/services/km/service/CrossTagsService.java
@@ -59,21 +59,21 @@ public class CrossTagsService extends BaseService {
 
 	/**
 	 * 
-	 * @param sourcetag
+	 * @param sourcetags
 	 * @param targettagset
 	 * @param httpRequest
 	 * @return
 	 */
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    public CrossTagResponse getCrossTags(@QueryParam("sourcetag") String sourcetag,
+    public CrossTagResponse getCrossTags(@QueryParam("sourcetags") String sourcetags,
     		@QueryParam("targettagset") String targettagset,
     		@QueryParam("targettagset1") String targettagset1,
     		@QueryParam("targettagset2") String targettagset2,
     		@QueryParam("targettagset3") String targettagset3,
     		@Context HttpServletRequest httpRequest) {
 		LOGGER.info("Entering getCrossTags()");
-		LOGGER.debug("sourcetag: " + sourcetag);
+		LOGGER.debug("sourcetags: " + sourcetags);
 		LOGGER.debug("targettagset: " + targettagset);
 		LOGGER.debug("targettagset1: " + targettagset1);
 		LOGGER.debug("targettagset2: " + targettagset2);
@@ -86,20 +86,20 @@ public class CrossTagsService extends BaseService {
 			LOGGER.debug("Username: " + credentials[0]);
 			LOGGER.debug("Password: " + credentials[1]);
 
-			if (sourcetag == null || sourcetag.length() == 0 ||
+			if (sourcetags == null || sourcetags.length() == 0 ||
 				targettagset == null || targettagset.length() == 0) {
 				throw new AppException(500, AppErrorCodes.CROSS_TAGS_ERROR,  
 						AppErrorMessage.CROSS_TAGS_ERROR);			
 			} else {
-				final StringTokenizer st2 = new StringTokenizer(sourcetag, ",");
+				final StringTokenizer st2 = new StringTokenizer(sourcetags, ",");
 				if (st2 != null && st2.countTokens() > 0) {
-					String[] sourcetags = new String[st2.countTokens()];
+					String[] stags = new String[st2.countTokens()];
 					int x = 0;
 					while (st2.hasMoreTokens()) {
-						sourcetags[x++] = st2.nextToken(); 
+						stags[x++] = st2.nextToken(); 
 					}
 					// Get all the cross tag combinations
-					crossTagResponse = crossTagsDAO.getTagSetConfigurations(credentials[0], credentials[1], sourcetags, targettagset, targettagset1, targettagset2, targettagset3);
+					crossTagResponse = crossTagsDAO.getTagSetConfigurations(credentials[0], credentials[1], stags, targettagset, targettagset1, targettagset2, targettagset3);
 				}
 			}
 		} catch (SQLException sqle) {
