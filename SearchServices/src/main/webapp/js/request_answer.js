3b807b2c65984129e014ab649960e0e483875024
hbendale <hbendale@r932  hbendale  2015-03-17 11:42:00 -0500 (Tue, 17 Mar 2015)>
2017-01-10 23:18:30 +0000

    Fixed Request Answer for tags
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@2173 b552034e-f646-462b-9072-8d94b4fb3fa1

--- SearchServices/src/main/webapp/js/request_answer.js
+++ SearchServices/src/main/webapp/js/request_answer.js
@@ -9,6 +9,7 @@
 
 	// Submit button
 	$.fn.requestAnswerSubmit = function() {
+		$.fn.parseParams();
 		var searchDate = new Date();
 		var jsonDate = JSON.stringify(searchDate);
 		var expectation = $('#request-answer-expectation').val();
@@ -26,6 +27,77 @@
 				$('#popup').removeClass('popup_on');
 			});
 		}
+	}
+	
+	$.fn.parseParams = function() {
+		var tagList = $.fn.getAllParameters().tags.split(',');
+		var categoryList = $.fn.getAllParameters().categories.split(',');
+		if(tagList) {
+			tagList = tagList.filter(function(tags) {
+			    if(tags != "" && tags.includes('_'))
+			        return tags;
+			});
+		}
+		if(categoryList) {
+			categoryList = categoryList.filter(function(category) {
+			    if(category != "" && category.includes('_'))
+			        return category;
+			});
+		}
+		
+		selectedFilter = "AKSC_locale::English;;";
+		if(categoryList && categoryList.length > 0) {
+			selectedFilter += "AKSC_contentCategory::";
+			categoryList.forEach(function(category) {
+			    selectedFilter += document.getElementById(category).title + ",";
+			});
+			selectedFilter = selectedFilter.substring(0, selectedFilter.length - 1);
+			selectedFilter += ";;";
+		}
+		
+		if(tagList && tagList.length > 0) {
+			var kbaseTags = [], productTags = [], regionTags = [];
+			tagList.forEach(function(tag) {
+				if(tag.includes('kbase_')) {
+					kbaseTags.push(tag);
+				}
+				else if(tag.includes('product_')) {
+					productTags.push(tag);
+				}
+				else if(tag.includes('region_')) {
+					regionTags.push(tag);
+				}
+			});
+			
+			if(kbaseTags.length > 0) {
+				selectedFilter += "kbase::";
+				kbaseTags.forEach(function(tag) {
+				    selectedFilter += document.getElementById(tag).title + ",";
+				})
+			}
+			selectedFilter = selectedFilter.substring(0, selectedFilter.length - 1);
+			selectedFilter += ";;";
+			
+			if(productTags.length > 0) {
+				selectedFilter += "product::";
+				productTags.forEach(function(tag) {
+				    selectedFilter += document.getElementById(tag).title + ",";
+				})
+			}
+			selectedFilter = selectedFilter.substring(0, selectedFilter.length - 1);
+			selectedFilter += ";;";
+			
+			if(regionTags.length > 0) {
+				selectedFilter += "region::";
+				regionTags.forEach(function(tag) {
+				    selectedFilter += document.getElementById(tag).title + ",";
+				})
+			}
+			selectedFilter = selectedFilter.substring(0, selectedFilter.length - 1);
+			selectedFilter += ";;";
+		}
+		
+		selectedFilter += "status::Published;;";
 	}
 
 	// Cancel button
