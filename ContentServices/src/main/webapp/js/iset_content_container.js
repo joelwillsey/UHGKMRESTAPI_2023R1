494476a4de1a2337a7e1b514911360a6179c0b6c
pseifert <pseifert@r998  pseifert  2015-04-07 11:11:24 -0500 (Tue, 07 Apr 2015)>
2017-08-23 02:15:46 +0000

    Change the minimumscale t0 minimum-scale
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3322 b552034e-f646-462b-9072-8d94b4fb3fa1

--- ContentServices/src/main/webapp/js/iset_content_container.js
+++ ContentServices/src/main/webapp/js/iset_content_container.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
 	// Load content widget
-	$.get(contentServiceName + 'iset_content.html', function(data) {
-		$('#content-widget').html(data);
-	});
+	if ($.fn.isDraftContent()){
+		// Draft content might not be in the contentServiceName path because it is proxied so we need to get the proxy path to bypass Siteminder 
+		var contextPath = window.location.pathname.split('/');		 
+		/**When we split the pathname the first array element will be blank because it looks like this /verintkm/verintkm.html
+		contextPath[0]="", contextPath[1]="verintkm", contextPath[2]="verintkm.html", **/
+		var contextPath = window.location.pathname.split('/');
+		
+		var URL = '';
+		
+		if (contextPath.length > 1){
+			var newContextPath = '/';
+			//don't want the last element as it is the html page
+			for (x=0; x < contextPath.length-1; x++){
+				if(typeof contextPath[x] != 'undefined' && contextPath[x] != ''){
+					newContextPath = newContextPath + contextPath[x] + "/";
+				}
+			}
+			log("This is draft content retrieving pathname as it may be proxied. context=" + newContextPath  + "content.html");
+			$.get(newContextPath + 'iset_content.html', function(data) {
+				$('#content-widget').html(data);
+			});
+		} else {
+			$.get(contentServiceName + 'iset_content.html', function(data) {
+				$('#content-widget').html(data);
+			});
+		}
+	} else {
+
+		$.get(contentServiceName + 'iset_content.html', function(data) {
+			$('#content-widget').html(data);
+		});
 	
 	/*
 	** Copy to comment content.
@@ -26,5 +54,4 @@ $(document).ready(function() {
 			}
 			autoDocText="";
 		}
-	}
-});
+	}}});
