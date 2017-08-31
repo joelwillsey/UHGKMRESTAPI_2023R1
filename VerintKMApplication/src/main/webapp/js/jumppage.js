075791793578ee5af73c182aa01e189341660b38
pseifert <pseifert@r998  pseifert  2015-04-07 11:11:24 -0500 (Tue, 07 Apr 2015)>
2017-08-31 18:38:08 +0000

    iset changes to make the search work
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3344 b552034e-f646-462b-9072-8d94b4fb3fa1

--- VerintKMApplication/src/main/webapp/js/jumppage.js
+++ VerintKMApplication/src/main/webapp/js/jumppage.js
@@ -3,6 +3,9 @@
  */
 var searchServiceName = "/searchservices/";
 var contentServiceName = "/contentservices/";
+var filtersServiceName = '/filterservices/';
+var kbaseTags = null;
+
 //$(document).ready(function() {
 $.fn.setUpJump=function() {
 	var params = $.fn.getAllParametersString();
@@ -55,140 +58,146 @@ $.fn.setUpJump=function() {
 		log("ISET Variables: " + variables);
 		
 		
+		
 		//we want to check that the first element is create
 		if( variables[0] == "create"){
 			var knowledgeBase = "";
 			var split = variables[1].split(":");
 			knowledgeBase = split[1];
 			
-			switch(variables[2]) {
-			case "Search":
-				var Search = variables[3];
-				//run a search on knowledgeBase and a keyword test
-				var url = searchServiceName + "iset_search_container.html?tags=" + knowledgeBase + "&query="+Search;
-				window.location.replace(url);
-				break;
-			case "report":
-				if(variables[3].substring(0,2) == "p{"){
-					var report = variables[3].substring(2, variables[3].length-1);
-
-					//run a search for problem object on knowledgeBase and Problem GUID
-					jQuery.ajaxSetup({async:false});
-					var migratableReferenceId = $.fn.getIsetResponse("","SOLUTION",report);
-					jQuery.ajaxSetup({async:true});
-					var url = contentServiceName + 'iset_content_container.html?id='+migratableReferenceId;
+			if($.fn.checkKBaseTags(knowledgeBase)){
+				switch(variables[2]) {
+				case "Search":
+					var Search = variables[3];
+					//run a search on knowledgeBase and a keyword test
+					var url = searchServiceName + "iset_search_container.html?tags=" + knowledgeBase + "&query="+Search;
+					window.location.replace(url);
+					break;
+				case "report":
+					if(variables[3].substring(0,2) == "p{"){
+						var report = variables[3].substring(2, variables[3].length-1);
+	
+						//run a search for problem object on knowledgeBase and Problem GUID
+						jQuery.ajaxSetup({async:false});
+						var migratableReferenceId = $.fn.getIsetResponse("","SOLUTION",report);
+						jQuery.ajaxSetup({async:true});
+						var url = contentServiceName + 'iset_content_container.html?id='+migratableReferenceId;
+						window.location.replace(url);
+						break;
+						
+					}else if(variables[3].substring(0,2) == "p["){
+						var report = variables[3].substring(2, variables[3].length-1);
+						
+						//run a search for problem object on knowledgeBase and Problem RefID
+						jQuery.ajaxSetup({async:false});
+						var migratableReferenceId = $.fn.getIsetResponse(report,"SOLUTION","");
+						jQuery.ajaxSetup({async:true});
+						var url = contentServiceName + 'iset_content_container.html?id='+migratableReferenceId;
+						window.location.replace(url);
+						break;
+						
+					}else if(variables[3].substring(0,2) == "p("){
+						var report = variables[3].substring(2, variables[3].length-1);
+						
+						var url = searchServiceName + "iset_search_container.html?tags=" + knowledgeBase + ","+report;
+						window.location.replace(url);
+						break;
+						
+					}else if(variables[3].substring(0,2) == "d{"){
+						var report = variables[3].substring(2, variables[3].length-1);
+						
+						//run a search for decision tree on knowledgeBase and Dtree GUID
+						jQuery.ajaxSetup({async:false});
+						var migratableReferenceId = $.fn.getIsetResponse("","DOCUMENT",report);
+						jQuery.ajaxSetup({async:true});
+						var url = contentServiceName + 'iset_content_container.html?id=' + migratableReferenceId;
+						window.location.replace(url);
+						break;
+	
+					}else if(variables[3].substring(0,2) == "d["){
+						var report = variables[3].substring(2, variables[3].length-1);
+						
+						//run a search for decision tree on knowledgeBase and Dtree RefID
+						jQuery.ajaxSetup({async:false});
+						var migratableReferenceId = $.fn.getIsetResponse(report,"DOCUMENT","");
+						jQuery.ajaxSetup({async:true});
+						var url = contentServiceName + 'iset_content_container.html?id=' + migratableReferenceId;
+						window.location.replace(url);
+						break;
+						
+					}else if(variables[3].substring(0,2) == "d("){
+						var report = variables[3].substring(2, variables[3].length-1);
+						
+						//run a search for decision tree on knowledgeBase and Migratable Reference
+						var url = contentServiceName + 'iset_content_container.html?id=' + report;
+						window.location.replace(url);
+						break;
+						
+					}
+					break;
+				case "view()":
+					if(variables[3].substring(0,2) == "r{"){
+						var report = variables[3].substring(2, variables[3].length-1);
+						
+						//run a search for content on knowledgeBase and Case GUID
+						jQuery.ajaxSetup({async:false});
+						var migratableReferenceId = $.fn.getIsetResponse("","CASE",report);
+						jQuery.ajaxSetup({async:true});
+						var url = contentServiceName + 'iset_content_container.html?id=' + migratableReferenceId;
+						window.location.replace(url);
+						break;
+						
+					}else if(variables[3].substring(0,2) == "r["){
+						var report = variables[3].substring(2, variables[3].length-1);
+						
+						//run a search for content on knowledgeBase and Case RefID
+						jQuery.ajaxSetup({async:false});
+						var migratableReferenceId = $.fn.getIsetResponse(report,"CASE","");
+						jQuery.ajaxSetup({async:true});
+						var url = contentServiceName + 'iset_content_container.html?id=' + migratableReferenceId;
+						window.location.replace(url);
+						break;
+						
+					}else if(variables[3].substring(0,2) == "r("){
+						var report = variables[3].substring(2, variables[3].length-1);
+						
+						//run a search for content on knowledgeBase and Migratable Reference
+						var url = contentServiceName + 'iset_content_container.html?id=' + report;
+						window.location.replace(url);
+						break;
+						
+					}
+					break;
+				case "Report":
+					var reportSplit = variables[3].split(":");
+					var content = reportSplit[0].substring(2, reportSplit[0].length-1);
+					var contentFaq = reportSplit[1];
+					
+					//apply filter to result list with knowledgeBase, content, and contentFAQ
+					var url = searchServiceName + "iset_search_container.html?tags=" + knowledgeBase + "&categories="+content+"&query="+contentFaq;
 					window.location.replace(url);
 					break;
 					
-				}else if(variables[3].substring(0,2) == "p["){
-					var report = variables[3].substring(2, variables[3].length-1);
+				case "searchProperties":
 					
-					//run a search for problem object on knowledgeBase and Problem RefID
-					jQuery.ajaxSetup({async:false});
-					var migratableReferenceId = $.fn.getIsetResponse(report,"SOLUTION","");
-					jQuery.ajaxSetup({async:true});
-					var url = contentServiceName + 'iset_content_container.html?id='+migratableReferenceId;
-					window.location.replace(url);
+					var searchProperties = variables[3];
+					var search = variables[5];
+					var forward = variables[7];
+					
+					//apply filter to to a code definition search
+					
 					break;
 					
-				}else if(variables[3].substring(0,2) == "p("){
-					var report = variables[3].substring(2, variables[3].length-1);
+				default:
 					
-					var url = searchServiceName + "iset_search_container.html?tags=" + knowledgeBase + ","+report;
-					window.location.replace(url);
+					$.fn.disableSpinner();
+					$.fn.handleErrorText("URL is not in the correct format");
+				
 					break;
-					
-				}else if(variables[3].substring(0,2) == "d{"){
-					var report = variables[3].substring(2, variables[3].length-1);
-					
-					//run a search for decision tree on knowledgeBase and Dtree GUID
-					jQuery.ajaxSetup({async:false});
-					var migratableReferenceId = $.fn.getIsetResponse("","DOCUMENT",report);
-					jQuery.ajaxSetup({async:true});
-					var url = contentServiceName + 'iset_content_container.html?id=' + migratableReferenceId;
-					window.location.replace(url);
-					break;
-
-				}else if(variables[3].substring(0,2) == "d["){
-					var report = variables[3].substring(2, variables[3].length-1);
-					
-					//run a search for decision tree on knowledgeBase and Dtree RefID
-					jQuery.ajaxSetup({async:false});
-					var migratableReferenceId = $.fn.getIsetResponse(report,"DOCUMENT","");
-					jQuery.ajaxSetup({async:true});
-					var url = contentServiceName + 'iset_content_container.html?id=' + migratableReferenceId;
-					window.location.replace(url);
-					break;
-					
-				}else if(variables[3].substring(0,2) == "d("){
-					var report = variables[3].substring(2, variables[3].length-1);
-					
-					//run a search for decision tree on knowledgeBase and Migratable Reference
-					var url = contentServiceName + 'iset_content_container.html?id=' + report;
-					window.location.replace(url);
-					break;
-					
 				}
-				break;
-			case "view()":
-				if(variables[3].substring(0,2) == "r{"){
-					var report = variables[3].substring(2, variables[3].length-1);
-					
-					//run a search for content on knowledgeBase and Case GUID
-					jQuery.ajaxSetup({async:false});
-					var migratableReferenceId = $.fn.getIsetResponse("","CASE",report);
-					jQuery.ajaxSetup({async:true});
-					var url = contentServiceName + 'iset_content_container.html?id=' + migratableReferenceId;
-					window.location.replace(url);
-					break;
-					
-				}else if(variables[3].substring(0,2) == "r["){
-					var report = variables[3].substring(2, variables[3].length-1);
-					
-					//run a search for content on knowledgeBase and Case RefID
-					jQuery.ajaxSetup({async:false});
-					var migratableReferenceId = $.fn.getIsetResponse(report,"CASE","");
-					jQuery.ajaxSetup({async:true});
-					var url = contentServiceName + 'iset_content_container.html?id=' + migratableReferenceId;
-					window.location.replace(url);
-					break;
-					
-				}else if(variables[3].substring(0,2) == "r("){
-					var report = variables[3].substring(2, variables[3].length-1);
-					
-					//run a search for content on knowledgeBase and Migratable Reference
-					var url = contentServiceName + 'iset_content_container.html?id=' + report;
-					window.location.replace(url);
-					break;
-					
-				}
-				break;
-			case "Report":
-				var reportSplit = variables[3].split(":");
-				var content = reportSplit[0].substring(2, reportSplit[0].length-1);
-				var contentFaq = reportSplit[1];
-				
-				//apply filter to result list with knowledgeBase, content, and contentFAQ
-				var url = searchServiceName + "iset_search_container.html?tags=" + knowledgeBase + "&categories="+content+"&query="+contentFaq;
-				window.location.replace(url);
-				break;
-				
-			case "searchProperties":
-				
-				var searchProperties = variables[3];
-				var search = variables[5];
-				var forward = variables[7];
-				
-				//apply filter to to a code definition search
-				
-				break;
-				
-			default:
-				
-				$.fn.disableSpinner();
-				$.fn.handleErrorText("URL is not in the correct format");
-			
-				break;
+			} else {
+				//not allowed to see content as they don't have the correct kbase tag
+				log('Agent does not have the knowledge base team that contains the kbase tag: ' + knowledgeBase);
 			}
 		} else if (variables[0] == "displayByRefName"){
 			// /verintkm/jumppage.html?displayByRefName= + refName 
@@ -207,4 +216,33 @@ $.fn.setUpJump=function() {
 		}
 	}
 	
-	
+	$.fn.checkKBaseTags = function(systemTagName) {
+		var url = filtersServiceName + 'km/kbasetags';		
+		var result = false;
+		
+    	jQuery.ajaxSetup({
+			async : false
+		});
+    	
+		$.fn.serviceCall('GET', '', url, 15000, function(data) {
+			
+			var kbaseData = '';
+			if (typeof data.tags != 'undefined' && data.tags != null && data.tags.length > 0) {
+				for (var x = 0; x < data.tags.length; x++) {
+					if (typeof data.tags[x] != 'undefined' && data.tags[x] != null && data.tags[x].systemTagName.length > 0) {
+						//log('tag[' + x + '].systemTagName=' + data.tags[x].systemTagName);
+						if (data.tags[x].systemTagName == systemTagName){
+							result = true;
+							//log ('result = ' + result);
+							break;}
+						}
+					}
+				}
+		});
+
+		jQuery.ajaxSetup({
+			async : true
+		});
+		log ('Agent result of $.fn.checkKBaseTags(' + systemTagName + ') = '+ result);
+		return result
+	}	
