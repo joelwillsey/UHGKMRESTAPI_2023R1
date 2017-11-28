72661337da8af334b789d9f33cfca6298b09fedc
arumpf <arumpf@r4767  arumpf  2020-01-31 15:24:38 -0600 (Fri, 31 Jan 2020)>
2017-11-28 22:35:36 +0000

    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3524 b552034e-f646-462b-9072-8d94b4fb3fa1

---
+++ SharedInfrastructure/src/main/java/com/kana/contactcentre/services/model/KMBookmarkServiceV2Service_wsdl/KMBookmarkServiceV2Service.java
@@ -0,0 +1,20 @@
+/**
+ * KMBookmarkServiceV2Service.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl;
+
+public interface KMBookmarkServiceV2Service extends javax.xml.rpc.Service {
+
+/**
+ * Soap Service to expose API(s) to manage bookmarks and folders.
+ */
+    public java.lang.String getKMBookmarkServiceV2PortAddress();
+
+    public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.KMBookmarkServiceV2PortType getKMBookmarkServiceV2Port() throws javax.xml.rpc.ServiceException;
+
+    public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.KMBookmarkServiceV2PortType getKMBookmarkServiceV2Port(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
