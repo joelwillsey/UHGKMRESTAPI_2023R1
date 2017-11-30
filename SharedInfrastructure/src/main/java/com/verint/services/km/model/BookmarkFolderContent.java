6940b4ec8d4b3a405cc121ef54da3a28cd004774
pseifert <pseifert@r998  pseifert  2015-04-07 11:11:24 -0500 (Tue, 07 Apr 2015)>
2017-11-30 22:32:46 +0000

    BookmarkFolderContentskeleton
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3538 b552034e-f646-462b-9072-8d94b4fb3fa1

--- SharedInfrastructure/src/main/java/com/verint/services/km/model/BookmarkFolderContent.java
+++ SharedInfrastructure/src/main/java/com/verint/services/km/model/BookmarkFolderContent.java
@@ -1,5 +1,55 @@
 package com.verint.services.km.model;
 
-public class BookmarkFolderContent {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "BookmarkFolderContent")
+@XmlAccessorType(XmlAccessType.NONE)
+public class BookmarkFolderContent implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@XmlElement(nillable = true)
+	private String folderId;
+
+	@XmlElement(nillable = true)
+	private String localeName = "";
+	
+	@XmlElement(nillable = true)
+	private Date createdDate;
+	
+	@XmlElement(nillable=true)
+	private int sequenceNumber;
+		
+	@XmlElement(nillable=true)
+	private String title;
+		
+	@XmlElement(nillable=true)
+	private String parentFolderId;
+
+	@XmlElement(nillable=true)
+	private ArrayList<BookmarkedContentV2> bookmarkContentList = new ArrayList<BookmarkedContentV2>();
+
+	/**
+	 * Constructor
+	 */
+	public BookmarkFolderContent() {
+		super();
+	}
+
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "BookmarkFolderContent [folderId=" + folderId + ", title=" + title + ", localeName=" + localeName + ", createdDates=" + createdDate.toString()  
+				+ ", sequenceNumber=" + sequenceNumber + ", parentFolderIdr=" + parentFolderId + ", bookmarkContentList=" + bookmarkContentList.toString() +"]";
+	}
 }
