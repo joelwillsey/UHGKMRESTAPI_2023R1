9d6ae1b3ee958374f12186d49705c03c93b1d729
pseifert <pseifert@r998  pseifert  2015-04-07 11:11:24 -0500 (Tue, 07 Apr 2015)>
2017-11-30 22:24:40 +0000

    BookmarkedContentV2 skeleton
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3537 b552034e-f646-462b-9072-8d94b4fb3fa1

--- SharedInfrastructure/src/main/java/com/verint/services/km/model/BookmarkedContentV2.java
+++ SharedInfrastructure/src/main/java/com/verint/services/km/model/BookmarkedContentV2.java
@@ -1,5 +1,68 @@
 package com.verint.services.km.model;
 
-public class BookmarkedContentV2 {
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author pseifert
+ *
+ */
+@XmlRootElement(name = "BookmarkedContentV2")
+@XmlAccessorType(XmlAccessType.NONE)
+public class BookmarkedContentV2 implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@XmlElement(nillable = true)
+	private String synopsis;
+
+	@XmlElement(nillable = true)
+	private String localeName = "";
+	
+	@XmlElement(nillable = true)
+	private Date createdDate;
+	
+	@XmlElement(nillable=true)
+	private int sequenceNumber;
+	
+	@XmlElement(nillable=true)
+	private boolean isFeatured = false;
+	
+	@XmlElement(nillable=true)
+	private boolean isNewOrChanged = false;
+	
+	@XmlElement(nillable=true)
+	private String title;
+	
+	@XmlElement(nillable=true)
+	private String contentId;
+	
+	@XmlElement(nillable=true)
+	private String parentFolderId;
+
+
+	/**
+	 * Constructor
+	 */
+	public BookmarkedContentV2() {
+		super();
+	}
+
+	
+	
+	
+	
+	@Override
+	public String toString() {
+		return "BookmarkedContentV2 [contentIds=" + contentId + ", title=" + title + ", synopsis=" + synopsis
+				+ ", localeName=" + localeName + ", createdDates=" + createdDate.toString()  
+				+ ", isFeatured=" + isFeatured + ", isNewOrChanged=" + isNewOrChanged + ", sequenceNumber=" + sequenceNumber + ", parentFolderId=" + parentFolderId +"]";
+	}
 }
