c4d06bf0a427e91ca943ef83c5e9e7fb83f3d6b6
arumpf <arumpf@r4767  arumpf  2020-01-31 15:24:38 -0600 (Fri, 31 Jan 2020)>
2017-11-30 15:43:47 +0000

    Just updating the layout so init is at the top
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3531 b552034e-f646-462b-9072-8d94b4fb3fa1

--- SearchServices/src/main/java/com/kana/contactcentre/services/model/KMBookmarkServiceV2Service_wsdl/KMBookmarkServiceV2BindingImpl.java
+++ SearchServices/src/main/java/com/kana/contactcentre/services/model/KMBookmarkServiceV2Service_wsdl/KMBookmarkServiceV2BindingImpl.java
@@ -10,49 +10,51 @@ package com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl;
 import java.rmi.RemoteException;
 
 public class KMBookmarkServiceV2BindingImpl implements com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.KMBookmarkServiceV2PortType{
-	  private String _endpoint = null;
-	  private com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.KMBookmarkServiceV2PortType bookmarkServiceV2Port = null;
-	
+	private String _endpoint = null;
+	private com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.KMBookmarkServiceV2PortType bookmarkServiceV2Port = null;
+
+	private void _initBookmarksV2Port() {
+		try {
+			bookmarkServiceV2Port = (new com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.KMBookmarkServiceV2ServiceLocator()).getKMBookmarkServiceV2Port();
+			if (bookmarkServiceV2Port != null) {
+				if (_endpoint != null)
+					((javax.xml.rpc.Stub)bookmarkServiceV2Port)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+				else
+					_endpoint = (String)((javax.xml.rpc.Stub)bookmarkServiceV2Port)._getProperty("javax.xml.rpc.service.endpoint.address");
+			}
+
+		}
+		catch (javax.xml.rpc.ServiceException serviceException) {}
+	}
+
 	public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ManageBookmarksV2ResponseBodyType manageBookmarksV2(com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ManageBookmarksV2RequestBodyType body) throws java.rmi.RemoteException {
-        return null;
-    }
+		return null;
+	}
 
-    public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ListAllBookmarksV2ResponseBodyType listAllBookmarksV2(com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ListAllBookmarksV2RequestBodyType body) throws java.rmi.RemoteException {
-        if (bookmarkServiceV2Port == null)
-        	_initBookmarksV2Port();
-          return bookmarkServiceV2Port.listAllBookmarksV2(body);
-    }
+	public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ListAllBookmarksV2ResponseBodyType listAllBookmarksV2(com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ListAllBookmarksV2RequestBodyType body) throws java.rmi.RemoteException {
+		if (bookmarkServiceV2Port == null)
+			_initBookmarksV2Port();
+		return bookmarkServiceV2Port.listAllBookmarksV2(body);
+	}
 
-    public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.GetBookmarkResponseBodyType getBookmark(com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.GetBookmarkRequestBodyType body) throws java.rmi.RemoteException {
-        return null;
-    }
+	public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.GetBookmarkResponseBodyType getBookmark(com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.GetBookmarkRequestBodyType body) throws java.rmi.RemoteException {
+		return null;
+	}
 
-    public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.GetFolderResponseBodyType getFolder(com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.GetFolderRequestBodyType body) throws java.rmi.RemoteException {
-        return null;
-    }
+	public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.GetFolderResponseBodyType getFolder(com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.GetFolderRequestBodyType body) throws java.rmi.RemoteException {
+		return null;
+	}
 
-    public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ReorderBookmarkAndFolderResponseBodyType reorderBookmarkAndFolder(com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ReorderBookmarkAndFolderRequestBodyType body) throws java.rmi.RemoteException {
-        if (bookmarkServiceV2Port == null)
-        	_initBookmarksV2Port();
-    	return reorderBookmarkAndFolder(body);
-    }
-    private void _initBookmarksV2Port() {
-        try {
-        	bookmarkServiceV2Port = (new com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.KMBookmarkServiceV2ServiceLocator()).getKMBookmarkServiceV2Port();
-          if (bookmarkServiceV2Port != null) {
-            if (_endpoint != null)
-              ((javax.xml.rpc.Stub)bookmarkServiceV2Port)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
-            else
-              _endpoint = (String)((javax.xml.rpc.Stub)bookmarkServiceV2Port)._getProperty("javax.xml.rpc.service.endpoint.address");
-          }
-          
-        }
-        catch (javax.xml.rpc.ServiceException serviceException) {}
-      }
+	public com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ReorderBookmarkAndFolderResponseBodyType reorderBookmarkAndFolder(com.kana.contactcentre.services.model.KMBookmarkServiceV2Service_wsdl.ReorderBookmarkAndFolderRequestBodyType body) throws java.rmi.RemoteException {
+		if (bookmarkServiceV2Port == null)
+			_initBookmarksV2Port();
+		return reorderBookmarkAndFolder(body);
+	}
+
 	@Override
 	public ListAllBookmarksV2ResponseBodyType listAllV2(ListAllBookmarksV2RequestBodyType listallRequest) throws RemoteException {
-        if (bookmarkServiceV2Port == null)
-        	_initBookmarksV2Port();
+		if (bookmarkServiceV2Port == null)
+			_initBookmarksV2Port();
 		return listAllBookmarksV2(listallRequest);
 	}
 
