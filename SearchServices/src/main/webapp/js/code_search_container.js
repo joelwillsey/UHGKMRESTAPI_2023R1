87eb833468219e11b0b1160348635115f7dd7613
pseifert <pseifert@r998  pseifert  2015-04-07 11:11:24 -0500 (Tue, 07 Apr 2015)>
2018-02-13 16:58:54 +0000

    Changes to code order to fix undefined $.fn.setupSearchWidget(); (white screen)
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3739 b552034e-f646-462b-9072-8d94b4fb3fa1

--- SearchServices/src/main/webapp/js/code_search_container.js
+++ SearchServices/src/main/webapp/js/code_search_container.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+	//Resize Window
+	window.resizeTo(1024,440);
+	
+	//Move window to top right corner
+	var ow =  screen.width - window.outerWidth;	
+	window.moveTo(ow,0);
+	
 	// Load Search Results HTML
 	$.get(searchServiceName + 'code_search.html', function(data) {
 		$('#code-search-widget').html(data);
--- SearchServices/src/main/webapp/js/iset_search_container.js
+++ SearchServices/src/main/webapp/js/iset_search_container.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+	
+	//Resize Window
+	window.resizeTo(530,440);
+	
+	//Move window to top right corner
+	var ow =  screen.width - window.outerWidth;	
+	window.moveTo(ow,0);
+	
 	// Load Search Results HTML
 	$.get(searchServiceName + 'iset_search.html', function(data) {
 		$('#search-widget').html(data);
