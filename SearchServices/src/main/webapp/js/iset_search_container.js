b5b890a735b4c6bad9aa140ea9a1d664609f8271
pseifert <pseifert@r998  pseifert  2015-04-07 11:11:24 -0500 (Tue, 07 Apr 2015)>
2018-02-26 16:46:36 +0000

    Moved the window move controls to the last file loaded
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3767 b552034e-f646-462b-9072-8d94b4fb3fa1

--- SearchServices/src/main/webapp/js/iset_search_container.js
+++ SearchServices/src/main/webapp/js/iset_search_container.js
@@ -2,15 +2,6 @@ $(document).ready(function() {
 	
 	// Load Search Results HTML
 	$.get(searchServiceName + 'iset_search.html', function(data) {
-		$('#search-widget').html(data);
-		
-	//Resize Window
-	window.resizeTo(530,440);
-	
-	//Move window to top right corner
-	var ow =  screen.width - window.outerWidth;	
-	window.moveTo(ow,0);
-	
-
+		$('#search-widget').html(data);		
 	});
 });
