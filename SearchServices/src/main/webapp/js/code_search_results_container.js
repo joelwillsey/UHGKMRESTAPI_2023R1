143858043e56ceae59e875a7296c6ef0c8253bfc
pseifert <pseifert@r998  pseifert  2015-04-07 11:11:24 -0500 (Tue, 07 Apr 2015)>
2018-02-26 18:35:06 +0000

    Moved the window move controls to the last file loaded
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3768 b552034e-f646-462b-9072-8d94b4fb3fa1

--- SearchServices/src/main/webapp/js/code_search_results_container.js
+++ SearchServices/src/main/webapp/js/code_search_results_container.js
@@ -1,5 +1,14 @@
 
 $(document).ready(function() {
+	
+	//Resize Window
+	window.resizeTo(1024,440);
+	
+	//Move window to top right corner
+	var ow =  screen.width - window.outerWidth;	
+	window.moveTo(ow,0);
+	
+	
 	// Load Search Results HTML
 	$.get(searchServiceName + 'code_search_results.html', function(data) {
 		$('#search-results-widget').html(data);
