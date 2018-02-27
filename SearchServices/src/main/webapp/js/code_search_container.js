bcd28d30698b48ca7555db819420a1a4d71d0fae
pseifert <pseifert@r998  pseifert  2015-04-07 11:11:24 -0500 (Tue, 07 Apr 2015)>
2018-02-27 14:13:38 +0000

    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/KMRestAPI@3769 b552034e-f646-462b-9072-8d94b4fb3fa1

--- SearchServices/src/main/webapp/js/code_search_container.js
+++ SearchServices/src/main/webapp/js/code_search_container.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-	//Resize Window
-	window.resizeTo(1024,440);
-	
-	//Move window to top right corner
-	var ow =  screen.width - window.outerWidth;	
-	window.moveTo(ow,0);
 	
 	// Load Search Results HTML
 	$.get(searchServiceName + 'code_search.html', function(data) {
