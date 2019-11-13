aaed6a73d5ed9aa3affb465a8448db3acbf6e39d
ivize <ivize@r5614  ivize  2021-12-16 09:31:23 -0600 (Thu, 16 Dec 2021)>
2019-11-13 11:53:56 +0000

    U13U-191, Unable to view content with attachments in REST, Amended parser to look for double quotes in the href data instead of single quotes. Looks like this was changed from double to single quotes in May 2017 but needs to be returned after the upgrade.
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/branches/KMRestAPI_15_3_FP5@4280 b552034e-f646-462b-9072-8d94b4fb3fa1

--- ContentServices/src/main/java/com/verint/services/km/dao/parser/ElementParser.java
+++ ContentServices/src/main/java/com/verint/services/km/dao/parser/ElementParser.java
@@ -407,38 +407,40 @@ public class ElementParser {
 			LOGGER.debug("begin: " + begin);
 			LOGGER.debug("end: " + end);
 			if (begin != -1 && end != -1) {
-				int findBeginEnd = data.indexOf("'>");
+				int findBeginEnd = data.indexOf("\">");
 				LOGGER.debug("findBeginEnd: " + findBeginEnd);
 				if (findBeginEnd != -1){
 					Attachment attachment = new Attachment();
-					String name = data.substring(findBeginEnd + "'>".length(), end);
+					String name = data.substring(findBeginEnd + "\">".length(), end);
 					attachment.setName(name);
 					// Get href
-					//int beginHref = data.indexOf("href=\"");
-					int beginHref = data.indexOf("href='");
+					int beginHref = data.indexOf("href=\"");
+					//int beginHref = data.indexOf("href='");
 					LOGGER.debug("beginHref: " + beginHref);
 					if (beginHref != -1) {
-						//String tempString = data.substring(beginHref + "href=\"".length());
-						String tempString = data.substring(beginHref + "href='".length());
+						String tempString = data.substring(beginHref + "href=\"".length());
+						//String tempString = data.substring(beginHref + "href='".length());
 						LOGGER.debug("tempString: " + tempString);
-						//int endHref = tempString.indexOf("\">");
-						int endHref = tempString.indexOf("'>");
+						int endHref = tempString.indexOf("\">");
+						//int endHref = tempString.indexOf("'>");
 						LOGGER.debug("endHref: " + endHref);
 						if (endHref != -1) {
 							attachment.setUrl(tempString.substring(0, endHref));
 							attachments.add(attachment);
 							data = data.substring(end + 1);
 						} else {
-							//LOGGER.error("Unable to find '\">\" for closing of href tag.  Link Substring: " + data.substring(begin, end));
-							LOGGER.error("Unable to find '> for closing of href tag.  Link Substring: " + data.substring(begin, end));
+							LOGGER.error("Unable to find '\">\" for closing of href tag.  Link Substring: " + data.substring(begin, end));
+							//LOGGER.error("Unable to find '> for closing of href tag.  Link Substring: " + data.substring(begin, end));
+							break;
 						}
 					} else {
 						LOGGER.error("Unable to find href=' for start of link. Link Substring: " + data.substring(begin, end));
 						break;
 					}
 				}else {
-					//LOGGER.error("Unable to find '\">\" for closing of end tag of <a  findBeginEnd: " + findBeginEnd);
-					LOGGER.error("Unable to find '> for closing of end tag of <a  findBeginEnd: " + findBeginEnd);
+					LOGGER.error("Unable to find '\">\" for closing of end tag of <a  findBeginEnd: " + findBeginEnd);
+					//LOGGER.error("Unable to find '> for closing of end tag of <a  findBeginEnd: " + findBeginEnd);
+					break;
 				}
 			} else {
 				break;
