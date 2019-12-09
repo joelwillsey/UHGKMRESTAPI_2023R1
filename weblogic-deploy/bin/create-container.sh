e4a1a6ebc7bd0fd77171cbe86fb2c63faa3e2697
rmacdonald <rmacdonald@r4855  rmacdonald  2020-02-20 21:02:06 -0600 (Thu, 20 Feb 2020)>
2019-12-09 17:16:51 +0000

    Richard - initial commit of first SH script to test what works/doesn't
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/branches/KMRestAPI_15_3_FP5@4342 b552034e-f646-462b-9072-8d94b4fb3fa1

---
+++ weblogic-deploy/bin/create-container.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+set +v
+
+echo
+echo ==========================
+echo Create the KMRestAPI Container
+echo ==========================
+echo
+
+if [$1 = ""]; then
+	blank()
+fi
+
+# Variable Assignments
+CONTAINER_NAME=$1
+FINISHED_MESSAGE="BUILD FAILED"
+
+# Check if the environment varaibles have been exported (only need to be run once per session)
+if [$KM_CONFIG_ENVIRONMENT_expor != "true"]; then
+    echo exporting config environment variables
+	echo Running script ../config/export-config.sh
+	(exec ../config/export-config.sh)
+	if [ERRORLEVEL = 1]; then
+		config_error()
+	fi
+fi	
+# export the container environment variables every time
+echo
+echo exporting container environment variables
+echo
+
+DIRECTORY = ../config/$KM_ENVIRONMENT_NAME/$KM_MACHINE_NAME/container.$CONTAINER_NAME/
+if [! -d DIRECTORY]; then
+	environment_error();
+	else
+	echo Running script ../config/$KM_ENVIRONMENT_NAME/$KM_MACHINE_NAME/container.$CONTAINER_NAME/export-container-environment.sh
+	(exec ../config/$KM_ENVIRONMENT_NAME/$KM_MACHINE_NAME/container.$CONTAINER_NAME/export-container-environment.sh)
+	if [ERRORLEVEL = 1]; then
+		environment_error()
+	fi
+fi	
+echo Creating contianer $CONTAINER_NAME
+echo command is $KM_WLS_HOME/common/bin/wlst.sh ../scripts/create-container.py
+echo
+
+(exec $KM_WLS_HOME/common/bin/wlst.sh ../scripts/create-container.py)
+
+if [ERRORLEVEL = 1]; then 
+	error()
+fi
+
+DIRECTORY = $KM_MW_HOME/domains/server_$KM_DOMAIN/servers/$KM_DOMAIN
+if [! -d DIRECTORY]; then
+	mkdir $KM_MW_HOME/domains/server_$KM_DOMAIN/servers/$KM_DOMAIN
+	if [ERRORLEVEL = 1 ]; then
+		error()
+	fi
+	mkdir $KM_MW_HOME/domains/server_$KM_DOMAIN/servers/$KM_DOMAIN/security
+	if [ERRORLEVEL = 1 ]; then
+		error()
+	fi
+    echo copying $KM_MW_HOME/domains/server_$KM_DOMAIN/servers/AdminServer/security $KM_MW_HOME/domains/server_$KM_DOMAIN/servers/$KM_DOMAIN/security
+    echo
+    cp --recursive --force $KM_MW_HOME/domains/server_$KM_DOMAIN/servers/AdminServer/security $KM_MW_HOME/domains/server_$KM_DOMAIN/servers/$KM_DOMAIN/security	
+	if [ERRORLEVEL = 1 ]; then
+		error()
+	fi
+fi
+
+# HAPPY PATH
+FINISHED_MESSAGE="COMMAND SUCCESSFUL"
+done()	
+
+function blank() {
+	echo Missing container name parameter [create-container.sh containerName]
+	error()
+}
+
+function mkdir_error() {
+	echo Unable to create new server directory
+	error()
+}
+
+function copy_error() {
+	echo Unable to copy security folder to new server
+	error()
+}
+
+function config_error() {
+	echo Unable to export the config and environment variables
+	error()
+}
+
+function environment_error() {
+	echo $CONTAINER_NAME is not a valid container name or the export-container-environment.sh file is missing. Path: ../config/$KM_ENVIRONMENT_NAME/$KM_MACHINE_NAME/container.$CONTAINER_NAME/export-container-environment.sh
+	error()
+}
+
+function error() {
+	FINISHED_MESSAGE="BUILD FAILED"
+	done()
+}
+
+function done() {
+	echo $FINISHED_MESSAGE
+	exit
+}
