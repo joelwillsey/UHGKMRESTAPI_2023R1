846a05c1ae4bb327877f4d81316a1aab92954bfb
rmacdonald <rmacdonald@r4855  rmacdonald  2020-02-20 21:02:06 -0600 (Thu, 20 Feb 2020)>
2019-12-10 16:46:17 +0000

    Richard - adding error handling for property lookup
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/branches/KMRestAPI_15_3_FP5@4357 b552034e-f646-462b-9072-8d94b4fb3fa1

--- weblogic-deploy/bin/ccadmin.sh
+++ weblogic-deploy/bin/ccadmin.sh
@@ -120,7 +120,12 @@ function setContainerProperties () {
 function getProperty () {
 	PROP_KEY=$1
 	PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
-	echo $PROP_VALUE
+	if [ ! "$?" = "0" ]; then 
+		echo "Error finding property file: ${PROPERTYFILE}"
+		endScriptFail	
+	else
+		echo $PROP_VALUE
+	fi	
 }
 
 # =====================
