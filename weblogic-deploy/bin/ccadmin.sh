b0bf072bf2cfd2d27565db7f58ef2f7edd5654c7
rmacdonald <rmacdonald@r4855  rmacdonald  2020-02-20 21:02:06 -0600 (Thu, 20 Feb 2020)>
2019-12-10 15:38:47 +0000

    Richard - re-writing ccadmin script to pick up properties for environment/container, cleaning up old cmd/sh scripts for just DEV
    git-svn-id: https://drive.kana.com/SVN/UHG/IQ/branches/KMRestAPI_15_3_FP5@4353 b552034e-f646-462b-9072-8d94b4fb3fa1

--- weblogic-deploy/bin/ccadmin.sh
+++ weblogic-deploy/bin/ccadmin.sh
@@ -56,63 +56,93 @@ function setConfig () {
 		echo "Setting environment to ${ENVIRONMENT_NAME}"
 		echo "Setting machine to ${KM_MACHINE_NAME}"
 		echo "Your config environment has been exported"
-		echo "---"
-		echo "Running Set Environment"
-		setEnvironment
-		echo "Successfully exported the config and environment variables"
 	fi 	
 }
 
 function setEnvironment () {
 	# Path the em-appserver, export only KM_MW_HOME
-	export KM_MW_HOME="/app_2/verint/em/containers/em-appserver"
-	export KM_WLS_HOME="${KM_MW_HOME}/wlserver"
-		
+	KM_MW_HOME="/app_2/verint/em/containers/em-appserver"
+	KM_WLS_HOME="${KM_MW_HOME}/wlserver"
+	export $KM_MW_HOME
+	export $KM_WLS_HOME
+	
 	# Path to war files export KM_RELEASE_DIRECTORY
-	export KM_RELEASE_DIRECTORY="/app_2/verint/em/projects/uhgiq/restapi/release"
-		
+	KM_RELEASE_DIRECTORY="/app_2/verint/em/projects/uhgiq/restapi/release"
+	export $KM_RELEASE_DIRECTORY
+	
 	# em-appserver logging directories
-	export EM_KM_BASE_LOG_PATH="/app_2/verint/em/logs"
-	export EM_SERVER_LOGS="${EM_KM_BASE_LOG_PATH}/${KM_DOMAIN}/weblogic/server.log"
-	export EM_ACCESS_LOGS="${EM_KM_BASE_LOG_PATH}/${KM_DOMAIN}/weblogic/http-access.log"
-	export EM_DIAGNOSTIC_LOGS="${EM_KM_BASE_LOG_PATH}/${KM_DOMAIN}/weblogic/diagnostic-images"
-		
-	export EM_IDENTITY_KEYSTORE="/app_2/verint/em/products/agent-desktop_15.3-FP6_5.6.0/config/config-exports/environment/environment.abstract-local-dev/resources/local-dev-keystore.jks"
-	export EM_IDENTITY_STORE_PASSPHRASE="changeit"
-	export EM_TRUST_KEYSTORE="/app_2/verint/em/products/agent-desktop_15.3-FP6_5.6.0/config/config-exports/environment/environment.abstract-local-dev/resources/local-dev-keystore.jks"
-	export EM_TRUST_STORE_PASSPHRASE="changeit"
+	EM_KM_BASE_LOG_PATH="/app_2/verint/em/logs"
+	EM_SERVER_LOGS="${EM_KM_BASE_LOG_PATH}/${KM_DOMAIN}/weblogic/server.log"
+	EM_ACCESS_LOGS="${EM_KM_BASE_LOG_PATH}/${KM_DOMAIN}/weblogic/http-access.log"
+	EM_DIAGNOSTIC_LOGS="${EM_KM_BASE_LOG_PATH}/${KM_DOMAIN}/weblogic/diagnostic-images"
+	export $EM_KM_BASE_LOG_PATH
+	export $EM_SERVER_LOGS
+	export $EM_ACCESS_LOGS
+	export $EM_DIAGNOSTIC_LOGS
 	
 	# This is the java options the weblogic.WLST runs with
-	export JAVA_WLST_OPTIONS="-Dweblogic.security.IdentityKeyStore=CustomIdentity -Dweblogic.security.CustomIdentityKeyStoreFileName=${EM_IDENTITY_KEYSTORE} -Dweblogic.security.CustomIdentityKeyStorePassPhrase=${EM_IDENTITY_STORE_PASSPHRASE} -Dweblogic.security.Identity.KeyStoreType=JKS -Dweblogic.security.TrustKeyStore=CustomTrust -Dweblogic.security.CustomTrustKeyStoreFileName=${EM_TRUST_KEYSTORE} -Dweblogic.security.CustomTrustKeyStoreType=JKS -Dweblogic.security.CustomTrustKeyStorePassPhrase=${EM_TRUST_STORE_PASSPHRASE} -Dweblogic.security.IgnoreHostNameVerification=true -Dweblogic.security.SSL.ignoreHostnameVerification=true"
-	
-	# container start up options
-	export KM_STARTUP_OPTIONS=" -Denvironment.name=local -DconfigLocation=/app_2/verint/em/projects/uhg/KMRestAPI/kmservices/config -Dmachine.name=localhost -Dcontainer.name=dev -DlogFile=/app_2/verint/em/logs"
-	export KM_MEMORY_OPTS=" -Xms1024m -Xmx1024m"
-	export ORIGINAL_JAVA_OPTIONS=$JAVA_OPTIONS
+	JAVA_WLST_OPTIONS="-Dweblogic.security.IdentityKeyStore=CustomIdentity -Dweblogic.security.CustomIdentityKeyStoreFileName=${EM_IDENTITY_KEYSTORE} -Dweblogic.security.CustomIdentityKeyStorePassPhrase=${EM_IDENTITY_STORE_PASSPHRASE} -Dweblogic.security.Identity.KeyStoreType=JKS -Dweblogic.security.TrustKeyStore=CustomTrust -Dweblogic.security.CustomTrustKeyStoreFileName=${EM_TRUST_KEYSTORE} -Dweblogic.security.CustomTrustKeyStoreType=JKS -Dweblogic.security.CustomTrustKeyStorePassPhrase=${EM_TRUST_STORE_PASSPHRASE} -Dweblogic.security.IgnoreHostNameVerification=true -Dweblogic.security.SSL.ignoreHostnameVerification=true"
+	export $JAVA_WLST_OPTIONS
 
+	# container start up options
+	KM_STARTUP_OPTIONS=" -Denvironment.name=${ENVIRONMENT_NAME} -DconfigLocation=/app_2/verint/em/projects/uhg/KMRestAPI/kmservices/config -Dmachine.name=${KM_MACHINE_NAME} -Dcontainer.name=${CONTAINER_NAME} -DlogFile=/app_2/verint/em/logs"
+	ORIGINAL_JAVA_OPTIONS=$JAVA_OPTIONS
+
+	export $KM_STARTUP_OPTIONS
+	export $ORIGINAL_JAVA_OPTIONS
 	if [ ERRORLEVEL = 1 ]; then 
 		echo "Error in setEnvironment()"
+		endScriptFail
+	fi
+	echo "Successfully exported the environment variables"	
+}
+
+function setEnvironmentProperties () {
+	PROPERTY_FILE="../config/environment.${ENVIRONMENT_NAME}/environment.properties"
+	echo "# Reading properties from $PROPERTY_FILE"
+	
+	EM_IDENTITY_KEYSTORE=$(getProperty "em.identity.keystore")
+	EM_IDENTITY_STORE_PASSPHRASE=$(getProperty "em.identity.store.passphrase")
+	EM_TRUST_KEYSTORE=$(getProperty "em.trust.keystore")
+	EM_TRUST_STORE_PASSPHRASE=$(getProperty "em.trust.store.passphrase")
+	KM_MEMORY_OPTS=$(getProperty "km.memory.opts")
+	export $EM_IDENTITY_KEYSTORE
+	export $EM_IDENTITY_STORE_PASSPHRASE
+	export $EM_TRUST_KEYSTORE
+	export $EM_TRUST_STORE_PASSPHRASE
+	export $KM_MEMORY_OPTS
+}
+
+function setContainer () {
+	KM_DOMAIN=$CONTAINER_NAME
+	echo Container (domain) has been exported to [$KM_DOMAIN]
+	echo Your container environment has been exported.	
+	if [ ERRORLEVEL = 1 ]; then 
+		echo "Error in setContainer()"
 		endScriptFail
 	fi	
 }
 
-function setContainerEnvironment () {
-	# EM-AppServer connection info
-	export KM_MANAGEMENT_USERNAME="kmappservermanager"
-	export KM_MANAGEMENT_PASSWORD="kmappserver123"
-	export KM_CONNECT_URL="t3s://localhost:10290"
-	export KM_EM_APPSERVER_PORT=8680
+function setContainerProperties () {
+	PROPERTY_FILE="../config/environment.${ENVIRONMENT_NAME}/${KM_MACHINE_NAME}/container.${CONTAINER_NAME}/container.properties"
+	echo "# Reading properties from $PROPERTY_FILE"
 	
-	# container start up options (This is optional if export at the Environment level)
-	# export KM_STARTUP_OPTIONS= -Denvironment.name=local -DconfigLocation=C:\em\projects\uhg\KMRestAPI\kmservices\config -Dmachine.name=localhost -Dcontainer.name=dev -DlogFile=C:\app_2\verint\projects\uhgiq\restapi\logs
-	# export KM_MEMORY_OPTS= -Xms1024m -Xmx1024m
-	KM_DOMAIN=$CONTAINER_NAME
-	echo "Container (domain) has been exported to $KM_DOMAIN"
-	echo Your container environment has been exported.	
-	if [ ERRORLEVEL = 1 ]; then 
-		echo "Error in setContainerEnvironment()"
-		endScriptFail
-	fi	
+	KM_MANAGEMENT_USERNAME=$(getProperty "km.management.username")
+	KM_MANAGEMENT_PASSWORD=$(getProperty "km.management.password")
+	KM_CONNECT_URL=$(getProperty "km.connect.url")
+	KM_EM_APPSERVER_PORT=$(getProperty "km.em.appserver.port")
+	
+	export $KM_MANAGEMENT_USERNAME
+	export $KM_MANAGEMENT_PASSWORD
+	export $KM_CONNECT_URL
+	export $KM_EM_APPSERVER_PORT
+
+}
+
+function getProperty () {
+	PROP_KEY=$1
+	PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+	echo $PROP_VALUE
 }
 
 # =====================
@@ -139,9 +169,15 @@ echo "Started running setConfig()"
 setConfig
 echo "Finished running setConfig()"
 echo "---"
-echo "Started running setContainerEnvironment()"
-setContainerEnvironment
-echo "Finshed running setContainerEnvironment()"
+echo "Started running setEnvironment()"
+setEnvironment
+setEnvironmentProperties
+echo "Finished running setEnvironment()"
+echo "---"
+echo "Started running setContainer()"
+setContainer
+setContainerProperties
+echo "Finshed running setContainer()"
 echo "---"
 echo "Running command: ${COMMAND_NAME}"
 (exec ../scripts/$COMMAND_NAME.sh)
